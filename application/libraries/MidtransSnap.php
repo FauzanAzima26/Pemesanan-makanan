d5bd45f8f49e4c61d1eb64fabb6a88817cc5901d
Fauzan Azima <azimafauzan266@gmail.com>
2025-06-27 23:35:50 +0700

    membuat .env

--- application/libraries/MidtransSnap.php
+++ application/libraries/MidtransSnap.php
@@ -4,7 +4,7 @@ require_once APPPATH . '../vendor/autoload.php';
 class MidtransSnap {
     public function __construct()
     {
-        \Midtrans\Config::$serverKey = 'Mid-server-3GvsI0VRkPbHij-6qFqqFHRo';
+        \Midtrans\Config::$serverKey = getenv('MIDTRANS_SERVER_KEY');
         \Midtrans\Config::$isProduction = false;
         \Midtrans\Config::$isSanitized = true;
         \Midtrans\Config::$is3ds = true;
